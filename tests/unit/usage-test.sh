3664cdf5811e563c18e8ce3ed9f41b86848fd9a9
Attila Somogyi <zsolt.attila.somogyi@gmail.com>
2022-04-18 19:44:17 +0200

    test: add unset variable test

--- tests/unit/usage-test.sh
+++ tests/unit/usage-test.sh
@@ -3,7 +3,15 @@ Describe 'usage test'
     Include ./tests/unit/include/usage-message.sh
     Include ./src/vcard2qrcode
     It 'usage() test'
-    When call usage ''
+    When run usage ''
         The stderr should eq "$USAGE_MESSAGE"
+    End
+    unset_variable() { unset program_name; }
+    Before 'unset_variable'
+    It 'usage() program_name is not set test'
+    When run usage usage ''
+        The status should be failure
+        The stdout should eq ""
+        The stderr should eq "program_name is not set."
     End
 End
