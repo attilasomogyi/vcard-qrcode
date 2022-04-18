af014c4fd12ae27eaeb9f769dd26e333228d0e7c
Attila Somogyi <zsolt.attila.somogyi@gmail.com>
2022-04-18 17:08:05 +0200

    test: fix usage test

--- tests/usage-test.sh
+++ tests/usage-test.sh
@@ -1,10 +1,8 @@
 #shellcheck shell=sh
-Describe 'vcard2qrcode'
-  Include ./src/vcard2qrcode
-  Describe 'usage()'
-  It 'usage test'
-  When call usage ''
-      The stderr should eq 'Usage: vcard2qrcode [ -i | --input FILENAME ] [ -o | --output FILENAME ]'
+Describe 'usage test'
+    Include ./src/vcard2qrcode
+    It 'usage() test'
+    When call usage ''
+        The stderr should eq 'Usage: vcard2qrcode [ -i | --input FILENAME ] [ -o | --output FILENAME ]'
     End
-  End
 End
