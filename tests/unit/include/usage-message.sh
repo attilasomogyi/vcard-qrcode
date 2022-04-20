9158d7dc9992cd4c75464375e062d17b3fc06518
Attila Somogyi <zsolt.attila.somogyi@gmail.com>
2022-04-20 21:03:39 +0200

    test: fix export variable

--- tests/unit/include/usage-message.sh
+++ tests/unit/include/usage-message.sh
@@ -1,2 +1,2 @@
 #shellcheck shell=sh disable=SC2034
-USAGE_MESSAGE="Usage: vcard2qrcode [ -i | --input FILENAME ] [ -o | --output FILENAME ]"
+export USAGE_MESSAGE="Usage: vcard2qrcode [ -i | --input FILENAME ] [ -o | --output FILENAME ]"
